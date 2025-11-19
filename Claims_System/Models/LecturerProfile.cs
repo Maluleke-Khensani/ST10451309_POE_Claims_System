5ca0628e754895a46abcc0acdfe368ea9e548729
Maluleke-Khensani <165706638+Maluleke-Khensani@users.noreply.github.com>
2025-11-20 00:44:35 +0200

    Build Lecturer Profile and Report submission UI + UX
    Created Razor page for lecturers to View there profile and the Reports. Added fields, layout, improved visuals, and intuitive flow following rubric design requirements.

---
+++ Claims_System/Models/LecturerProfile.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Claims_System.Models
+{
+    public class LecturerProfile
+    {
+        [Key]
+        public string UserId { get; set; }  
+
+
+        public string FullName { get; set; }    
+
+        [Required]
+        public string Email { get; set; }       
+
+        [Required]
+        public decimal HourlyRate { get; set; } // only lecturers need this
+
+        [ForeignKey("UserId")]
+        public ApplicationUser User { get; set; }
+
+
+    }
+}
---
+++ Claims_System/Models/LecturerProfileViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Claims_System.Models;
+using System.Collections.Generic;
+
+namespace Claims_System.Models
+{
+    public class LecturerProfileViewModel
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public decimal HourlyRate { get; set; }
+
+        public List<LecturerClaim> Claims { get; set; } = new List<LecturerClaim>();
+    }
+}
