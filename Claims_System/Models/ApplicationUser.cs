96a4dde684d971a777728dafd0e05ad8e64d20b5
Maluleke-Khensani <165706638+Maluleke-Khensani@users.noreply.github.com>
2025-11-20 00:20:00 +0200

    Added a Styling and navigation for all pages
    Added styling in the _Layout page so it effects all the UIs .
    Updated the logo to fit within frame

---
+++ Claims_System/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Claims_System.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FullName { get; set; }
+
+        public virtual LecturerProfile? LecturerProfile { get; set; }
+
+
+    }
+}
